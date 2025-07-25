097285d7166b22f5bbb1e761ac79d5ee59e492e3
Hirwa Arrnaud <hirwatrevor8@gmail.com>
2025-07-25 16:37:13 +0200

    or action

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("hello", name,"Welcome to the game" )
 
 const ShouldWePlay = prompt("Do you  want to play  ")
 
-if (ShouldWePlay.toLowerCase() === "yes") {
+if (ShouldWePlay.toLowerCase() === "yes" || yes === "yeah") {
     const lang = prompt("Between USA and China which country have more population ")
     if (lang.toLowerCase() === "usa") {
         console.log("You are Wrong")
@@ -13,7 +13,7 @@ if (ShouldWePlay.toLowerCase() === "yes") {
             console.log("Correct")
         }
 }
-else if (ShouldWePlay.toLowerCase() === "no"){
+else if (ShouldWePlay.toLowerCase() === "no" || no === "nah"){
     console.log("You missed the game")
 }
 else{
